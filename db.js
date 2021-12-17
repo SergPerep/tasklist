1591db1d858a1d1cb09f227270476e2c5195ed0c
Sergei Perepelkin <perepelkin.serge@gmail.com>
2021-12-17 22:30:15 +0100

    Added configuration options to database connection

--- db.js
+++ db.js
@@ -4,11 +4,20 @@ const Pool = require("pg").Pool;
 // Access to .env variables
 require("dotenv").config();
 
-const pool = new Pool({
+const devConfig = {
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE
-});
+};
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL, // from heroku addons
+    ssl: {
+        rejectUnauthorized: false,
+    }
+};
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
 module.exports = pool;
