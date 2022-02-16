6f8b280e937910576e9e1996dba00e925520bb29
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-02-16 19:56:43 +0100

    Moved css-files into styles-directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react"
-import './App.css';
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import MainPage from "./components/Pages/MainPage";
 import LoginPage from "./components/Pages/LoginPage";
