623fd75bbff8a0b00bdc06d503ae8044e24a6ef6
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-03-03 20:41:11 +0100

    Tweaked design a little bit

--- client/src/components/Tasks/ModalForDeleteTask.js
+++ client/src/components/Tasks/ModalForDeleteTask.js
@@ -2,19 +2,20 @@ import Modal from "../Modals/Modal";
 import deleteTask from "../../fetch/deleteTask";
 
 const ModalForDeleteTask = ({ id, description, setIsModalOpen }) => {
-    
-    return <Modal buttonList={[{
-        title: "Delete",
-        onClick: () => {
-            deleteTask(id);
-            setIsModalOpen(false);
+
+    return <Modal buttonList={[
+        {
+            title: "Close",
+            design: "outlined",
+            onClick: () => setIsModalOpen(false)
+        }, {
+            title: "Delete",
+            onClick: () => {
+                deleteTask(id);
+                setIsModalOpen(false);
+            }
         }
-    },
-    {
-        title: "Close",
-        design: "outlined",
-        onClick: () => setIsModalOpen(false)
-    }]}>
+    ]}>
         Delete task <b>{description}</b>?
     </Modal>
 }
