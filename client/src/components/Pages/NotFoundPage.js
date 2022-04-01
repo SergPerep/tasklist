ec3554a1583e1efa2abf488d383c6cbd7e3e2a25
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-04-01 20:15:30 +0200

    Added HomeTopNav and DeveloperBar to 404 page. Made 404 button 'To home page' to react on Enter key

--- client/src/components/Pages/NotFoundPage.js
+++ client/src/components/Pages/NotFoundPage.js
@@ -1,21 +1,37 @@
 import Button from "../BasicUI/Button";
 import { useNavigate } from "react-router-dom";
 import { ReactComponent as UFOKidnapping } from "../../img/UFOKidnapping.svg";
+import HomeTopNav from "../HomeTopNav";
+import DeveloperBar from "../DeveloperBar/DeveloperBar";
 
 const NotFoundPage = () => {
-    
+
+
     const navigate = useNavigate();
+    
+    const handleClickToHome = () => navigate("/");
+    const handleEnterToHome = e => {
+        if (e.key !== "Enter") return;
+        handleClickToHome();
+    }
 
     return (
         <div className="canvas not-found">
+            <HomeTopNav />
             <div className="not-found-container">
                 <UFOKidnapping />
                 <div className="content-wrapper">
                     <h1>Page does not exist</h1>
                     <p>Check the request or return to home page</p>
-                    <Button onClick={() => { navigate("/") }}>To home page</Button>
+                    <Button
+                        onClick={handleClickToHome}
+                        onKeyDown={handleEnterToHome}
+                    >
+                        To home page
+                    </Button>
                 </div>
             </div>
+            <DeveloperBar />
         </div>
     )
 }
