d7393d3b13224cb11709a5f80cf8b97f1ab14572
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-04-08 02:20:48 +0200

    Remove loggining session info

--- src/middlewares/requireAuth.ts
+++ src/middlewares/requireAuth.ts
@@ -2,7 +2,6 @@ import { AuthenticationError } from "../utils/customErrors";
 import { Request, Response, NextFunction } from "express";
 
 const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    console.log({ session: req.session });
     if (!req.session?.user?.userId) return next(new AuthenticationError());
     next();
 }
