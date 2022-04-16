e3334bb0307afc9f4c312c10d1fd8495242f1740
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-04-16 20:00:17 +0200

    Got rid of useless js files

--- src/middlewares/requireAuth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const customErrors_1 = require("../utils/customErrors");
-const requireAuth = (req, res, next) => {
-    if (!req.session?.user?.userId)
-        return next(new customErrors_1.AuthenticationError());
-    next();
-};
-exports.default = requireAuth;
--- src/utils/validateUsername.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (username) => {
-    const regex = /^[^\W\s][\w\-\.]+$/gi;
-    return regex.test(username);
-};
