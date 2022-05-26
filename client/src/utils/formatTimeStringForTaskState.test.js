66b0e678c6fe0d5c25e332ed70c3a2951cb782ec
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-05-26 21:42:32 +0200

    Fixed bug: Now fromatTimeStringForTaskState function assumes current date and considers clock change

--- client/src/utils/formatTimeStringForTaskState.test.js
+++ client/src/utils/formatTimeStringForTaskState.test.js
@@ -2,27 +2,27 @@ import formatTimeStringForTaskState from "./formatTimeStringForTaskState";
 import { WrongTypeError } from "./customErrors";
 
 test("Without time zone offset", () => {
-    expect(formatTimeStringForTaskState("04:00:00+00:00")).toBe("5:00");
+    expect(formatTimeStringForTaskState("04:00:00+00:00")).toBe("6:00");
 });
 
 test("With time zone offset", () => {
-    expect(formatTimeStringForTaskState("04:00:00+01:00")).toBe("4:00");
-    expect(formatTimeStringForTaskState("05:00:00+02:00")).toBe("4:00");
-    expect(formatTimeStringForTaskState("05:00:00-03:30")).toBe("9:30");
+    expect(formatTimeStringForTaskState("04:00:00+01:00")).toBe("5:00");
+    expect(formatTimeStringForTaskState("05:00:00+02:00")).toBe("5:00");
+    expect(formatTimeStringForTaskState("05:00:00-03:30")).toBe("10:30");
 });
 
 test("Empty string", () => {
-    expect(formatTimeStringForTaskState("")).toBe(null);
+    expect(formatTimeStringForTaskState("", "")).toBe(null);
 });
 
 test("NaN", () => {
-    expect(formatTimeStringForTaskState(NaN)).toBe(null);
+    expect(formatTimeStringForTaskState(NaN, NaN)).toBe(null);
 });
 
 test("Empty object", () => {
-    expect(() => formatTimeStringForTaskState({})).toThrowError(WrongTypeError);
+    expect(() => formatTimeStringForTaskState({}, {})).toThrowError(WrongTypeError);
 });
 
 test("Null", () => {
-    expect(formatTimeStringForTaskState(null)).toBe(null);
+    expect(formatTimeStringForTaskState(null, null)).toBe(null);
 });
--- client/src/utils/formatTimeStringForTaskState.ts
+++ client/src/utils/formatTimeStringForTaskState.ts
@@ -3,11 +3,17 @@ import { WrongTypeError } from "./customErrors";
 
 /* HH:mm:ssZZ -> H:mm */
 
-const formatTimeStringForTaskState = (timeStr: string /* 16:00+01:00 */) => {
+const formatTimeStringForTaskState = (timeStr: string /* 16:00+01:00 */, nowObj = new Date()) => {
+    if (!nowObj) return null;
+    if (nowObj instanceof Date === false) throw new WrongTypeError("Date", nowObj, { nowObj });
+    
     if (!timeStr) return null;
     if (typeof timeStr !== "string") throw new WrongTypeError("string", timeStr, { timeStr });
-
-    return date.transform(timeStr, "HH:mm:ssZZ", "H:mm");
+    
+    const dateStr = date.format(nowObj, "YYYY-MM-DD");
+    const dateAndTimeStr =`${dateStr} ${timeStr}`;
+    
+    return date.transform(dateAndTimeStr, "YYYY-MM-DD HH:mm:ssZZ", "H:mm");
 }
 
 export default formatTimeStringForTaskState;
