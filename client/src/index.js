bc01df19b3b212c8583be79d2258fd04eafd7a4b
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-12-06 15:53:10 +0100

    Feat: stop css generation, work with scss only

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './css/style.css';
+import './scss/style.scss';
 import App from "./App";
 import { StoreProvider } from 'easy-peasy';
 import store from "./store";
